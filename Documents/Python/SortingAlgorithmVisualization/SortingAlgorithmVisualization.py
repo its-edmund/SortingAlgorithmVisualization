6fae2351d796a11cab6b8b9c57fed635c78f473f
mxinburritos <mxinburritos@gmail.com>
2018-12-28 16:36:29 -0800

    Bubble Sort works and its sorts. All of the drawing and animations work but no colors have been added.

--- Documents/Python/SortingAlgorithmVisualization/SortingAlgorithmVisualization.py
+++ Documents/Python/SortingAlgorithmVisualization/SortingAlgorithmVisualization.py
@@ -1,19 +1,25 @@
 import random
+import time
 from tkinter import *
 
 
 class Sorter:
+    bar_display = None
     height = 600
     width = 800
     randList = []
+    master = None
 
     def __init__(self, master):
         self.master = master
-        master.title("Sorting")
-        master.geometry(str(self.width) + "x" + str(self.height))
-        master.config(background='black')
+        self.bar_display = Canvas(master, width=self.width, height=self.height, highlightthickness=0, bg="black")
+        self.master.title("Sorting")
+        self.master.geometry(str(self.width) + "x" + str(self.height))
+        self.master.config(background='black')
         self.randList = self.generate_list(100)
-        self.draw(self.randList, master)
+        print(self.randList)
+        self.draw(self.randList)
+        self.bubble_sort(self.randList)
 
     def generate_list(self, num):
         list = []
@@ -23,26 +29,34 @@ class Sorter:
                 list.append(rand)
         return list
 
-    def draw(self, list, window):
-        canvas = Canvas(window, width=self.width, height=self.height, highlightthickness=0, bg="black")
+    def draw(self, list):
+        self.bar_display.delete("all")
         for num in list:
-            canvas.create_rectangle(list.index(num) * (self.width / len(list)),
-                                    self.height - (num * self.height / len(list)),
-                                    list.index(num) * (self.width / len(list)) + (
-                                            self.width / len(list)), self.height, fill='white')
+            self.bar_display.create_rectangle(list.index(num) * (self.width / len(list)),
+                                              self.height - (num * self.height / len(list)),
+                                              list.index(num) * (self.width / len(list)) + (
+                                                      self.width / len(list)), self.height, fill='white')
+            self.bar_display.pack()
+        self.master.update()
 
-            """list.index(num) * (self.width / len(list)), 150, list.index(num) * (self.width / len(list)) + (
-                        len(list) / self.width), 0, fill = 'red'"""
 
-            canvas.pack()
-
-    def process_algorithm(self, algorithm_name, list):
-        pass
+    def bubble_sort(self, list):
+        length_list = len(list)
+        while (length_list != 0):
+            new_length_list = 0
+            self.draw(list)
+            self.bar_display.after(50)
+            for index in range(1, length_list):
+                if list[index-1] > list[index]:
+                    temp = list[index-1]
+                    list[index-1] = list[index]
+                    list[index] = temp
+                    new_length_list = index
+            length_list = new_length_list
 
 def main():
     root = Tk()
     my_gui = Sorter(root)
-    print(my_gui.randList)
     root.mainloop()
 
 
